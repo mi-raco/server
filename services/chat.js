d48bd025b482214bf72fd9b56f3a281336aeddcd
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2023-12-05 21:54:24 +0000

    test

--- services/chat.js
+++ services/chat.js
@@ -1,5 +1,6 @@
 const openaiAPI = require('../integrations/openaiAPI');
 const dataAPI = require('../integrations/mongoDataAPI');
+const readline = require('readline');
 
 module.exports = {
   async addMessage(content, role, thread_id, data=dataAPI) {
@@ -37,24 +38,36 @@ module.exports = {
     return completion;
   },
 
-  async addChatResponse(content, system_instructions, thread_id) {
+  async addChatResponse(content, system_instructions, thread_id, openAICompletion) {
     await this.addMessage(content, "user", thread_id);
     const completion = await this.createCompletion(
       system_instructions,
       thread_id,
-      openaiAPI.completionSystemLast
+      openAICompletion
     );
     return completion;
   }
 };
 
 async function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
   const thread = await module.exports.newThread();
-  const system_instructions = "You are a grumpy old sailor. We are both in a restaurant and I just spilled your coffee." +
-                              "Don't break character, read the entire conversation history and respond succintly, be in the moment.";
-  const content = "Hey there, sorry I didn't see you there. I'll get you a new coffee.";
-  const response = await module.exports.addChatResponse(content, system_instructions, thread.insertedId);
-  console.log(response);
+  let system_instructions = "<context> You are a real human being from 1910, France. We are engaged in a conversation and I am about to reply. We are both roleplaying and supposed to build the story together."+
+                            "<task> Think about the entire conversation history, and respond in the most genuine way to your character. "+
+                            "<style> Your lexicon and articulation style should be genuine. Talk like a normal human being, with breaks and hesitations. "+ 
+                            "Don't break character and really embody this person fully. Keep a sense of self-preservation, act suprised or distressed if I reveal anything that may elicit such a reaction. "+
+                            "<creativity> Take control and dictate what happens next in the plot if the situation asks for it, or if I request it specifically. "+
+                            "The length of your answer should be chosen to be any number of words in the 7-40 range, with an average of 10, as the situation requires. ";
+  rl.setPrompt('\nLearner: ');
+  rl.prompt();
+  rl.on('line', async (userInput) => {
+    const response = await module.exports.addChatResponse(userInput, system_instructions, thread.insertedId, openaiAPI.completionSystemFirst);
+    console.log('\nAssistant: ' + response);
+    rl.prompt();
+  });
 }
 
 if (require.main === module) {
