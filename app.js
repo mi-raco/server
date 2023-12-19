e9fb37144007621498b74429c7738a66e846d154
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2023-12-19 01:27:25 +0100

    next

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var viewRouter = require('./routes/view');
 var openaiRouter = require('./routes/openai');
 var apiRouter = require('./routes/api');
 
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', viewRouter);
 app.use('/openai', openaiRouter);
 app.use('/api', apiRouter);
 
--- routes/api.ts
+++ routes/api.ts
@@ -1,10 +1,6 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
+export default express.Router().use(express.json())
+.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
-
-export default router;
