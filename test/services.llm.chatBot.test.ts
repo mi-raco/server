849cbcb9c9237ef882b0413d616a4d6d50f1c56f
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2023-12-27 06:21:18 +0000

    fixed test

--- test/services.llm.chatBot.test.ts
+++ test/services.llm.chatBot.test.ts
@@ -1,12 +1,13 @@
 import { expect } from './testSetup'
-import chatBot from '../services/threads';
+import completions from '../services/completions';
+import threads from '../services/threads';
 
 describe('chat service', () => {
   it('adds a message to a thread', async () => {
-    const newThread = await chatBot.newThread();
+    const newThread = await threads.newThread();
     const message = "Hello World";
     const role = "user";
-    const response = await chatBot.addMessageToThread(
+    const response = await threads.addMessageToThread(
       message,
       role,
       newThread.insertedId
@@ -17,11 +18,11 @@ describe('chat service', () => {
 
   it('creates a completion', async function() {
     this.timeout(50000);
-    const thread = await chatBot.newThread();
+    const thread = await threads.newThread();
     const system_instructions = "You are a grumpy old sailor. We are both in a restaurant and I just spilled your coffee." +
                                 "Don't break character, read the entire conversation history and respond succintly, be in the moment.";
     const content = "Hey there, sorry I didn't see you there. I'll get you a new coffee.";
-    const response = await chatBot.addChatResponse(content, system_instructions, thread.insertedId);
+    const response = await completions.createCompletionFromUserInput(content, system_instructions, thread.insertedId);
     console.log(response);
     expect(response).to.exist;
   });
