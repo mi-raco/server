1e0e1b967107e0cd5251568aa95035aa482e1d63
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2023-12-27 10:15:35 +0000

    llm endpoint

--- routes/llm.ts
+++ routes/llm.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from 'express';
 import promptVersions from '../services/promptVersions';
+import completions from '../services/completions';
 
 
 export default express.Router().use(express.json())
 .post('/action', async (req: Request, res: Response) => {
-  const { endUserId, actionId, parameters} = req.body;
+  const { actionId, parameters } = req.body;
   const promptVersion = await promptVersions.getLivePromptVersion(actionId);
-  
-
+  const user_input = promptVersions.populateUserInputTemplate(promptVersion, parameters);
+  const response = await completions.createCompletionFromUserInput(
+    user_input, 
+    promptVersion.system_instructions,
+    parameters.thread_id ?? undefined
+  );
+  res.json(response);    
 });
