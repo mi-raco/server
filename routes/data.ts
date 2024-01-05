ed59e444060834e6b5bcae7dd81a6dbcf2c70efe
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2024-01-05 20:19:15 +0200

    var name change

--- routes/data.ts
+++ routes/data.ts
@@ -3,8 +3,8 @@ import { RequestData, dataMap } from '../services/integrations/mongoDataAPI';
 
 export default express.Router().use(express.json())
 .post('/', async (req: Request, res: Response) => {
-  const action: string = req.body.action;
+  const dataAction: string = req.body.dataAction;
   const data: RequestData = req.body.data;
-  const response = await dataMap[action](data)
+  const response = await dataMap[dataAction](data)
   return res.status(200).json(response);
 });
